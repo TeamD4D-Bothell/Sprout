084066169aa23ba0fc449a08bedb8c00751c0643
Connor McGwire <cdmcgwire@live.com>
2016-04-29 21:35:18 -0700

    Added some basic Platformer setup
    The thin platform isn't working right now, but here you go.

---
+++ TeamD4D_Sprout/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public string horizInputString = "Horizontal";
+	public string jumpKey = "Jump";
+	public float speed = 5f;
+	public float jumpHeight = 4f;
+
+	private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputVal = Input.GetAxis(horizInputString);
+		if (inputVal != 0) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime * inputVal, 
+								Space.World);
+		}
+
+		if (Input.GetButtonDown(jumpKey)) {
+			rb2D.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
+		}
+	}
+}
