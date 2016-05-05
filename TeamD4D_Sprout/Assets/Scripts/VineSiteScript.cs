7ae02286a0ce6ee1aa19afc6bdbcede51686453d
Connor McGwire <cdmcgwire@live.com>
2016-05-04 21:36:19 -0700

    Added Michael and Spencer's projects

---
+++ TeamD4D_Sprout/Assets/Scripts/VineSiteScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VineSiteScript : MonoBehaviour {
+
+    private bool grow = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "VineSeed")
+        {
+            other.transform.position = this.transform.position;
+            grow = true;
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            rb.isKinematic = true;
+        }
+    }
+}
