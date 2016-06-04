9b3ef011a964710ca4d892566d41334db8a97891
Connor McGwire <cdmcgwire@live.com>
2016-06-03 21:14:01 -0700

    Menu works now

---
+++ TeamD4D_Sprout/Assets/Scripts/UI/EscMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscMenu : MonoBehaviour {
+
+	private GameObject menu;
+	private float initialTimeScale;
+	
+	void Awake() {
+		var menuRect = GetComponentInChildren<RectTransform>(true);
+		menu = menuRect.gameObject;
+		menu.SetActive(false);
+		initialTimeScale = Time.timeScale;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(menu.activeInHierarchy) {
+			if (Input.GetButtonDown("Cancel")) {
+				menu.SetActive(false);
+				Time.timeScale = initialTimeScale;
+			}
+		}
+		else {
+			if (Input.GetButtonDown("Cancel")) {
+				menu.SetActive(true);
+				Time.timeScale = 0;
+			}
+		}
+	}
+}
